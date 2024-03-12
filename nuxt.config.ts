bebecb9299c2f140a04709808e49501c8923cffa
zhang771 <19939926438@163.com>
2024-03-12 11:27:06 +0800

    chore(init): init project

---
+++ nuxt.config.ts
@@ -0,0 +1,44 @@
+import { pwa } from './config/pwa'
+
+export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+    '@vite-pwa/nuxt',
+  ],
+
+  experimental: {
+    payloadExtraction: false,
+    renderJsonPayloads: true,
+    typedPages: true,
+  },
+
+  css: [
+    '@unocss/reset/tailwind.css',
+  ],
+
+  colorMode: {
+    classSuffix: '',
+  },
+
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
+    },
+    prerender: {
+      crawlLinks: false,
+      routes: ['/'],
+      ignore: ['/hi'],
+    },
+  },
+
+  pwa,
+
+  devtools: {
+    enabled: true,
+  },
+})
